82475d165bcc640ec43c7f2382d463e85ee40a4f
astronomicle <719168@student.dusd.net>
2023-11-29 13:52:32 -0800

    finsihed vector but accedently restart started calculating distance
    i finished vector and started to work on calculating distance but deleted vector and restarted it

---
+++ MathematicsOfAI/Assets/Vectors/Move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update() 
+    {
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}

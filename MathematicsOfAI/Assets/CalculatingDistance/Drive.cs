94718bc14ef4aa969ea2f604ff76e6d23319a1f1
astronomicle <719168@student.dusd.net>
2023-11-29 19:16:34 -0800

    fixed vectors and completed rest of project
    I fixed the vectors issue and continued on and completed everything in this project

--- MathematicsOfAI/Assets/CalculatingDistance/Drive.cs
+++ MathematicsOfAI/Assets/CalculatingDistance/Drive.cs
@@ -9,9 +9,69 @@ public class Drive : MonoBehaviour
     public float speed = 10.0f;
     public float rotationSpeed = 100.0f;
 
+    public GameObject fuel;
+    bool autoPilot = false;
+
     void Start()
     {
 
+    }
+
+    void CalculateAngle()
+    {
+        Vector3 tF = this.transform.up;
+        Vector3 fD = fuel.transform.position - this.transform.position;
+
+        float dot = tF.x * fD.x + tF.y * fD.y;
+        float angle = Mathf.Acos(dot / (tF.magnitude * fD.magnitude));
+
+        Debug.Log("Angle: " + angle * Mathf.Rad2Deg);
+        Debug.Log("Unity Angle: " + Vector3.Angle(tF, fD));
+
+        Debug.DrawRay(this.transform.position, tF * 10, Color.green, 2);
+        Debug.DrawRay(this.transform.position, fD, Color.red, 2);
+
+        int clockwise = 1;
+        if (Cross(tF, fD).z < 0)
+            clockwise = -1;
+
+        float unityAngle = Vector3.SignedAngle(tF, fD, this.transform.forward);
+
+        this.transform.Rotate(0, 0, (angle * clockwise * Mathf.Rad2Deg) * 0.02f);
+    }
+
+    Vector3 Cross(Vector3 v, Vector3 w)
+    {
+        float xMult = v.y * w.z - v.z * w.y;
+        float yMult = v.z * w.x - v.x * w.z;
+        float zMult = v.x * w.y - v.y * w.x;
+
+        Vector3 crossProd = new Vector3(xMult, yMult, zMult);
+        return crossProd;
+    }
+
+    float CalculateDistance()
+    {
+        Vector3 tP = this.transform.position;
+        Vector3 fP = fuel.transform.position;
+
+        float distance = Mathf.Sqrt(
+                            Mathf.Pow(tP.x - fP.x, 2) + Mathf.Pow(tP.y - fP.y, 2) + Mathf.Pow(tP.z - fP.z, 2));
+
+        float unityDistance = Vector3.Distance(tP, fP);
+
+        Debug.Log("Distance: " + distance);
+        Debug.Log("Unity Distance: " + unityDistance);
+        return distance;
+    }
+
+    float autoSpeed = 0.1f;
+    void AutoPilot()
+    {
+        //this.transform.LookAt(fuel.transform.position, selfTransform.up);
+
+        CalculateAngle();
+        this.transform.Translate(this.transform.up * autoSpeed, Space.World);
     }
 
     void Update()
@@ -32,5 +92,20 @@ public class Drive : MonoBehaviour
         // Rotate around our y-axis
         transform.Rotate(0, 0, -rotation);
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CalculateDistance();
+            CalculateAngle();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            autoPilot = !autoPilot;
+        }
+        if (autoPilot)
+        {
+            if(CalculateDistance() > 5)
+                AutoPilot();
+        } 
     }
 }
--- MathematicsOfAI/Assets/Vectors/MoveToGoal.cs
+++ MathematicsOfAI/Assets/Vectors/MoveToGoal.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MoveToGoal : MonoBehaviour
 {
+    public float speed = 2.0f;
+    public float accuracy = 0.01f;
+    public Transform goal;
+
     void Start()
     {
         
@@ -11,6 +16,10 @@ public class MoveToGoal : MonoBehaviour
 
     void Update()
     {
-        
+        this.transform.LookAt(goal.position);
+        Vector3 direction = goal.position - this.transform.position;
+        Debug.DrawRay(this.transform.position, direction, Color.red);
+        if (direction.magnitude > accuracy)
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
     }
 }
